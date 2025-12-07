f1f7797f862666814094e79a565cf8c114106125
William Michael Kanar <kanarwm@outlook.com>
2025-12-06 20:25:27 -0500

    Swerve Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,6 +15,28 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static final class ModuleConstants {
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4.0);
+
+    public static final double kDriveMotorGearRatio = 6.75 / 1.0;
+    public static final double kTurningMotorGearRatio = 150.0 / 7.0;
+
+    public static final double kDriveEncoderRot2Meters = (2.0 * Math.PI) * (kWheelDiameterMeters / 2.0) / kDriveMotorGearRatio;
+    public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2.0 * Math.PI;
+
+    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meters / 60.0;
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60.0;
+
+    public static final double kPTurning = 0.0;
+    public static final double kITurning = 0.0;
+    public static final double kDTurning = 0.0;
+  }
+
+  public static final class DriveConstants {
+    public static final double kTrackWidth = Units.inchesToMeters(0.0);
+  }
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
