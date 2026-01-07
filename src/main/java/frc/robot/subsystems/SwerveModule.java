dfba37d2469bf550947a6e47ce5efbff740f38b0
Athena Lieu <athenaaabelle@gmail.com>
2026-01-07 11:01:09 -0500

    More swerve updates

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -13,11 +13,16 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.ModuleConstants;
 
 public class SwerveModule extends SubsystemBase {
@@ -86,6 +91,120 @@ public class SwerveModule extends SubsystemBase {
         Mechanism2d mod = new Mechanism2d(6, 6);
         MechanismRoot2d root = mod.getRoot("climber", 3, 3);
       }
+
+  public void update() {
+    SmartDashboard.putString(moduleName + " Position", getPosition().toString());
+  }
+
+  // Helpful get methods
+  public double getDrivePosition() {
+    return driveEncoder.getPosition();
+  }
+
+  public double getTurningPosition() {
+    return turningEncoder.getPosition();
+    // return getAbsoluteEncoderRad();
+  }
+
+  public double getDriveVelocity() {
+    return driveEncoder.getVelocity();
+  }
+
+  public double getTurningVelocity() {
+    return turningEncoder.getVelocity();
+  }
+
+  public SwerveModulePosition getPosition() {
+    return (new SwerveModulePosition(getDrivePosition(), new Rotation2d(getTurningPosition())));
+  }
+
+  public double getAbsoluteEncoderRad() {
+
+    // Make angle variable
+    double angle;
+
+    // Get encoder absolute position goes from 1 to 0
+    angle = (1 - absoluteEncoder.getAbsolutePosition()) * 2.0 * Math.PI;
+
+    angle -= absoluteEncoderOffsetRad;
+
+    return angle;
+  }
+
+  // Set turning encoder to match absolute encoder value with gear offsets applied
+  public void resetEncoders() {
+    driveEncoder.setPosition(0);
+    REVLibError error = turningEncoder.setPosition(getAbsoluteEncoderRad());
+    if (error.value != REVLibError.kOk.value) {
+    System.out.println("ENCODER ERROR ON " + error.value + " " + moduleName);
+    }
+  }
+
+  // Get swerve module current state, aka velocity and wheel rotation
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+  }
+  
+    public void setDesiredState(SwerveModuleState state) {
+
+    // Check if new command has high driving power
+    if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+      stop();
+      return;
+    }
+
+    state = SwerveModuleState.optimize(state, getState().angle);
+
+    driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+
+    turningMotor
+        .set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+
+    // simTurn.setAngle(state.angle);
+    // simDirection.setAngle(state.speedMetersPerSecond > 0 ? 0 : 180);
+
+    // simTurn2.setAngle(absoluteEncoder.getAbsolutePosition());
+    // simDirection2.setAngle(state.speedMetersPerSecond /
+    // DriveConstants.kPhysicalMaxSpeedMetersPerSecond > 0 ? 0 : 180);
+    SmartDashboard.putString(moduleName + " state", state.toString());
+
+  }
+
+  public void setDesiredState(SwerveModuleState state) {
+    // Check if new command has high driving power
+    if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+      stop();
+      return;
+    }
+    state = SwerveModuleState.optimize(state, getState().angle);
+
+    driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+
+    turningMotor
+        .set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+        SmartDashboard.putString(moduleName + " state", state.toString());
+
+  }
+
+  // Stop all motors on module
+  public void stop() {
+    driveMotor.set(0);
+    turningMotor.set(0);
+  }
+
+  // Motor and SparkMax methods for Monitor
+  public double[] getMotorsCurrent() {
+    return (new double[] { driveMotor.getOutputCurrent(), turningMotor.getOutputCurrent() });
+  }
+
+  public double[] getMotorsTemp() {
+    return (new double[] { driveMotor.getMotorTemperature(), turningMotor.getMotorTemperature() });
+  }
+
+  public void setSmartCurrentLimiter(int driveLimit, int turningLimit) {
+    driveMotor.setSmartCurrentLimit(driveLimit);
+    turningMotor.setSmartCurrentLimit(driveLimit);
+  }
   
   /** Creates a new SwerveModule. */
   public SwerveModule() {}
