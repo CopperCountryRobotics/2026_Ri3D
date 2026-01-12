bff05e41e24734233f4b74e0d94f40073863629c
cjalfrey <cjalfrey@mtu.edu>
2026-01-11 22:00:11 -0500

    remove unused imports

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,22 +1,13 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.PersistMode;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
-import com.lumynlabs.devices.ConnectorXAnimate;
 import com.lumynlabs.devices.ConnectorX;
 import com.lumynlabs.connection.usb.USBPort;
-import com.lumynlabs.connection.uart.UARTPort;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import static frc.robot.Constants.HardwareConstants.*;
 
 public class LEDSubsystem extends SubsystemBase {
     private final ConnectorX cX;
